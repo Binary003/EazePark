9b80dbc9020182dbd9c434b414979ecbac3d9d16
Anuj Verma <138612580+Binary003@users.noreply.github.com>
2025-05-16 04:16:10 +0530

    Update Signuppage.jsx

--- frontend/src/pages/Signuppage.jsx
+++ frontend/src/pages/Signuppage.jsx
@@ -21,7 +21,7 @@ const SignupPage = ({ onLoginClick }) => {
       return;
     }
 
-    const response = await fetch("http://localhost:5000/signup", {
+    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/signup`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
